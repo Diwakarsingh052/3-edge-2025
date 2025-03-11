fd2d5428e8aa06fce7bc91e2d4f00f0247a98789
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2025-03-11 11:46:56 +0530

    .

--- 1-concurrency/7-unbuf-question.go
+++ 1-concurrency/7-unbuf-question.go
@@ -1,26 +1,41 @@
 package main
 
-func main() {
-	add
-	sub
-	mult
+import (
+	"fmt"
+	"sync"
+)
 
-	go Add(1, 2)
-	go Sub(1, 2)
-	go Mult(1, 2)
-	go CollectResult()
+func main() {
+	add := make(chan int)
+	sub := make(chan int)
+	mult := make(chan int)
+	wg := new(sync.WaitGroup)
+	wg.Add(4)
+	go Add(1, 2, add, wg)
+	go Sub(1, 2, sub, wg)
+	go Mult(1, 2, mult, wg)
+	go CollectResult(add, sub, mult, wg)
+	wg.Wait()
 }
 
-func Add(a, b int) int {
+func Add(a, b int, ch chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	ch <- a + b
+
 	// send the result to the channel
 }
 
-func Sub(a, b int) int {}
-
-func Mult(a, b int) int {
-
+func Sub(a, b int, ch chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	ch <- a - b
 }
-func CollectResult() int {
 
+func Mult(a, b int, ch chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	ch <- a * b
+}
+func CollectResult(add, sub, mult chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println(<-add, <-sub, <-mult)
 	//recv all the values
 }
