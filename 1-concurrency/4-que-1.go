f34f06492364a0a2916520c5a34eb26b9f753e78
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2025-03-11 11:05:01 +0530

    .

--- 1-concurrency/4-que-1.go
+++ 1-concurrency/4-que-1.go
@@ -1,17 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-
+	wg := new(sync.WaitGroup)
 	for i := 1; i <= 4; i++ {
-		go work(i)
+		wg.Add(1)
+		go work(i, wg)
 	}
+
+	wg.Wait()
 
 }
 
-func work(workId int) {
+func work(workId int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		fmt.Println("doing a sub work", workId)
 	}()
 	fmt.Println("working", workId)
