5eda3cd1732e7378f4d7522e68011b2789ba2bd0
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2025-03-12 12:03:16 +0530

    .

--- 1-concurrency/13-mutex.go
+++ 1-concurrency/13-mutex.go
@@ -7,13 +7,19 @@ import (
 
 var x = 1
 
+// maps are not safe for concurrent access, use mutex for safe operations
+var user = map[int]string{
+	1: "john",
+}
+
 func main() {
 	wg := new(sync.WaitGroup)
 
 	m := new(sync.Mutex)
 	for i := 0; i < 2; i++ {
-		wg.Add(1)
+		wg.Add(2)
 		go UpdateX(i, wg, m)
+		go UpdateVariable(i, wg)
 	}
 
 	wg.Wait()
@@ -37,4 +43,13 @@ func UpdateX(val int, wg *sync.WaitGroup, m *sync.Mutex) {
 	defer m.Unlock()
 	x = val
 	fmt.Println("UpdateX:", x)
+}
+
+func UpdateVariable(val int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	// abc is a local variable, when we run n number goroutines we create n number of copies of abc
+	// so updating abc should not be a problem, because it would never be a concurrent writte in the below example
+	var abc int
+	abc = val
+	fmt.Println("UpdateVariable:", abc)
 }
