48eab55f29702984b8d72ec7d12392114fecf127
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2025-03-12 11:31:35 +0530

    .

--- 1-concurrency/12-select.go
+++ 1-concurrency/12-select.go
@@ -3,43 +3,46 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 func main() {
 
 	wg := new(sync.WaitGroup)
 	wgWorker := new(sync.WaitGroup)
-	get := make(chan string)
-	post := make(chan string)
-	put := make(chan string)
+	get := make(chan string, 2)
+	post := make(chan string, 2)
+	put := make(chan string, 2)
 	done := make(chan struct{})
 
 	wgWorker.Add(3)
 	go func() {
 		defer wgWorker.Done()
-		time.Sleep(3 * time.Second)
 		get <- "get done"
 	}()
 
 	go func() {
 		defer wgWorker.Done()
-		time.Sleep(1 * time.Second)
 		post <- "post done"
 	}()
 
 	go func() {
 		defer wgWorker.Done()
 		put <- "put done"
-		put <- "put 2 done"
 	}()
 
-	close(done)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		wgWorker.Wait()
+		close(done)
+	}()
 	//	fmt.Println(<-get)
 	//	fmt.Println(<-post)
 	//	fmt.Println(<-put)
 
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		//for i := 0; i < 3; i++ // this version works, if you know exactly the number of values you would receive
 		for {
 
