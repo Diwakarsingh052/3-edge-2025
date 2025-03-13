c7a365ddfad20d4a2425aafa8a3497f7237071a2
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2025-03-13 11:00:39 +0530

    .

--- 1-concurrency/14-rw-mutex.go
+++ 1-concurrency/14-rw-mutex.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strconv"
 	"sync"
+	"time"
 )
 
 // Theater represents a theater with a specific number of seats
@@ -25,7 +26,7 @@ func (t *Theater) bookSeat(name string) {
 	if t.seats > 0 {
 		// Simulate a seat booking-making process
 		fmt.Println("Seat is available for", name)
-		//time.Sleep(2 * time.Second)
+		time.Sleep(2 * time.Second)
 		fmt.Println("Booking confirmed", name)
 
 		t.seats-- // Decrement available seats
@@ -33,6 +34,19 @@ func (t *Theater) bookSeat(name string) {
 	} else {
 		fmt.Println("No seats available for", name) // Inform that no seats are available
 	}
+}
+
+// checkSeats method checks the available Seats in the Theater
+func (t *Theater) checkSeats(wg *sync.WaitGroup) {
+	defer wg.Done() // Decrement the counter when checkSeats is done executing
+	// Acquire a lock for reading
+	t.rw.RLock()
+	// Releases the read lock when func completes
+
+	//no one can write when read lock is acquired,
+	// there could be unlimited number of reads but no writes allowed while reading
+	defer t.rw.RUnlock()
+	fmt.Println("Available Seats:", t.seats)
 }
 
 func (t *Theater) printInvoice(wg *sync.WaitGroup) {
@@ -48,9 +62,21 @@ func main() {
 		seats:   2,
 		invoice: make(chan string),
 	}
+
+	// Start checkSeat routines
+	for i := 1; i <= 6; i++ {
+		wg.Add(1) // Increment wait group counter
+		go t.checkSeats(wg)
+	}
 	for i := 0; i < 5; i++ {
 		wgBook.Add(1)
 		go t.bookSeat("User " + strconv.Itoa(i))
+	}
+
+	// Start checkSeat routines
+	for i := 1; i <= 6; i++ {
+		wg.Add(1) // Increment wait group counter
+		go t.checkSeats(wg)
 	}
 	wg.Add(1)
 	go t.printInvoice(wg)
