62852aece8e811f3487bb9734ec2fc2f96a63167
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2025-03-13 11:42:25 +0530

    .

--- 2-context/1-ctx.go
+++ 2-context/1-ctx.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -19,32 +20,49 @@ import (
 func main() {
 	//Background returns a non-nil, empty Context. we can put timer values in it
 	ctx := context.Background()
+	wg := new(sync.WaitGroup)
 	ch := make(chan int)
 
 	// putting the timeout value in the context container
-	ctx, cancel := context.WithTimeout(ctx, time.Second*4)
+	ctx, cancel := context.WithTimeout(ctx, time.Second*1)
 	defer cancel() // it would cleanup the resources taken up by the context
 
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		x := SlowFn()
-		ch <- x
+		select {
+		case ch <- x: // send
+			fmt.Println("value sent over the channel")
+		case <-ctx.Done():
+			fmt.Println("timeout happened, no longer we can send values")
+			fmt.Println("reverse the affect of SlowFunc")
+			return
+		}
+
 	}()
-	select {
-	// if received value in time, this case evaluates
-	case x := <-ch:
-		fmt.Println(x)
+	func() {
 
-		// listen over the done channel and if the time is up this case evaluates
-	case <-ctx.Done():
-		fmt.Println(ctx.Err())
-		fmt.Println("cancelling the goroutine")
-		return
-	}
+		select {
+		// if received value in time, this case evaluates
+		case x := <-ch:
+			fmt.Println(x)
 
+			// listen over the done channel and if the time is up this case evaluates
+		case <-ctx.Done():
+			fmt.Println(ctx.Err())
+			fmt.Println("cancelling the current function")
+			fmt.Println("no longer waiting for goroutine to finish")
+			return
+		}
+	}()
+
+	fmt.Println("Doing other important tasks .........")
+	wg.Wait()
 }
 
 func SlowFn() int {
-	time.Sleep(time.Second * 2)
+	time.Sleep(time.Second * 4)
 	fmt.Println("slowFunction: slow fn ran and add 100 records to db")
 	fmt.Println()
 	return 100
